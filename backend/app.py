6d221c62c417731800aa792010039e2e33f3f7c4
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-02-28 22:18:55 -0500

    Started the backend

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from openai import OpenAI
+
+client = OpenAI()
+app = Flask(__name__)
+
+
+@app.route("/transcript")
+def transcript():
+    """Send the original transcript."""
+    return {"message": "Success"}
+
+
+@app.route("/question")
+def hello_world():
+    """Send the question to the OpenAI API."""
+    return {"message": "Success"}
